46c890584074d4c232b38f4db09e7a1e67564e54
Cailenys <cailenys.salazar@gmail.com>
2020-10-29 11:43:38 -0600

    Creating category and product models

--- store/models.py
+++ store/models.py
@@ -6,6 +6,11 @@ class Category(models.Model):
     description = models.TextField(blank=True)
     image = models.ImageField(upload_to='category', blank=True)
 
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
     def _str_(self): 
       return self.name
 
@@ -13,4 +18,18 @@ class Product(models.Model):
     name = models.CharField(max_length=250, unique=True)
     slug= models.SlugField(max_length=250, unique=True)
     description = models.TextField(blank=True)
-    category = models.ForeignKey("Category", on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='product', blank=True)
+    stock = models.IntegerField()
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now = True)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'product'
+        verbose_name_plural = 'products'
+
+    def _str_(self):
+        return self.name
